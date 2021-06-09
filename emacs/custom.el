0a5d3420025dc513701288c956a432a4da0c2381
Joe Downs <joedowns502@gmail.com>
2021-06-09 18:52:03 -0400

    .emacs: move custom-set settings to custom.el

---
+++ emacs/custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-fill-column-indicator-character 124)
+ '(fill-column 80)
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   '(rainbow-identifiers rainbow-delimiters company-fuzzy company-c-headers company yaml yaml-mode json-mode banner-comment auctex web-mode linum-off fill-column-indicator latex-extra gh-md markdown-mode wucuo))
+ '(save-place-mode t)
+ '(scroll-down-aggressively 0.25)
+ '(scroll-margin 5)
+ '(send-mail-function 'smtpmail-send-it)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(truncate-lines t)
+ '(truncate-partial-width-windows nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal))))
+ '(custom-group-tag ((t (:inherit variable-pitch :foreground "color-184" :weight bold :height 1.2))))
+ '(custom-variable-tag ((t (:foreground "color-130" :weight bold))))
+ '(fill-column-indicator ((t (:foreground "red"))))
+ '(font-latex-sectioning-5-face ((t (:foreground "color-51" :weight bold))))
+ '(font-lock-builtin-face ((t (:foreground "color-96"))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-comment-face ((t (:foreground "color-70"))))
+ '(font-lock-doc-face ((t (:foreground "color-94"))))
+ '(font-lock-function-name-face ((t (:foreground "yellow"))))
+ '(font-lock-keyword-face ((t (:foreground "color-134"))))
+ '(font-lock-string-face ((t (:foreground "color-168"))))
+ '(font-lock-type-face ((t (:foreground "cyan"))))
+ '(minibuffer-prompt ((t (:foreground "brightcyan"))))
+ '(org-formula ((t (:foreground "color-77"))))
+ '(org-table ((t (:foreground "brightblue"))))
+ '(outline-1 ((t (:foreground "color-219"))))
+ '(outline-2 ((t (:foreground "color-210"))))
+ '(outline-3 ((t (:foreground "color-180"))))
+ '(outline-4 ((t (:foreground "color-108"))))
+ '(outline-5 ((t (:foreground "color-117"))))
+ '(outline-6 ((t (:foreground "color-69"))))
+ '(outline-7 ((t (:foreground "color-99"))))
+ '(outline-8 ((t (:foreground "color-134"))))
+ '(web-mode-html-tag-bracket-face ((t (:foreground "white")))))
