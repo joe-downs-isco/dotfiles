8a9f0886348d48b7d4836c319e5fb537aaeab0a7
Joe Downs <joe@dwns.dev>
2022-07-09 23:38:55 -0400

    keybinds.el: bind C-<return> to company-complete-selection

--- emacs/keybinds.el
+++ emacs/keybinds.el
@@ -2,3 +2,4 @@
 ;; This file stores all my manually-set keyboard shortcuts; redefining shortcuts
 ;; that don't quite work for me.
 (global-set-key (kbd "C-x g") 'goto-line)
+(global-set-key (kbd "C-<return>") 'company-complete-selection)
