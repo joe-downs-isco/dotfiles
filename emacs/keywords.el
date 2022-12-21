dd3e50523a236cd4264db5a188ffb577089b7cbd
Joe Downs <joe@dwns.dev>
2022-12-20 23:42:25 -0500

    add FIXME: and TODO: keywords to prog-mode

--- emacs/keywords.el
+++ emacs/keywords.el
@@ -3,3 +3,11 @@
 			;; Highlight variables in assignment, skip ==
 			`(("\\([[:graph:]]*\\)\\([[:blank:]]\\{1,\\}=\\{1\\}[[:blank:]]\\{1,\\}\\)\\([[:graph:]]\\)"
 			   1 font-lock-variable-name-face)))
+
+;;; Programming
+;; Keywords for every programming mode. Specific languages will follow in the
+;; form of sub headings
+(add-hook 'prog-mode-hook
+	  (lambda ()
+	    (font-lock-add-keywords nil `(("\\<\\(TODO\\|FIXME\\):"
+					   0 font-lock-warning-face prepend)))))
